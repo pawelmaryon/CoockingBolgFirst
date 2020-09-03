8c7a47861e519b330c2a1df0c56bc7b8b9ecd986
pawelmaryon <maryonpawel@gmail.com>
2020-09-03 14:46:16 +0100

    Seeds created, faker gem installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_130057) do
+ActiveRecord::Schema.define(version: 2020_09_03_130937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "portfolios", force: :cascade do |t|
     t.string "title"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do | blog |
+  Blog.create!(
+    title: Faker::Food.spice,
+    body: Faker::Food.description
+  )
+  end
+  9.times do |portfolio_item|
+    Portfolio.create!(
+      title: Faker::Food.dish,
+      description: Faker::Food.ingredient,
+      body: Faker::Food.description,
+      main_image: "http://placehold.it/600x400",
+      thumb_image: "http://placehold.it/350x200",
+    )
+  end
