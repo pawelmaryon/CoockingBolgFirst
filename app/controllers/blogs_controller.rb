de0e359a5288c1dd430f1977d27616ffa2d5bdf9
pawelmaryon <maryonpawel@gmail.com>
2020-09-04 22:22:10 +0100

    implemented all actions for blog, friendly_id gem installed its functionalities implemented to blog controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,47 @@
 class BlogsController < ApplicationController
+    def index
+      @blogs = Blog.all
+    end
+  
+    def new
+      @blog = Blog.new
+    end
+  
+    def create
+      @blog = Blog.new(blog_params)
+      if @blog.save
+        redirect_to portfolios_path
+      else
+        render :new
+      end
+    end
+  
+    def edit
+      @blog = Blog.friendly.find(params[:id])
+    end
+  
+    def update
+      @blog = Blog.friendly.find(params[:id])
+  
+      if @blog.update(blog_params)
+        redirect_to portfolios_path
+      else
+        render :edit
+      end
+    end
+    def show
+      @blog = Blog.friendly.find(params[:id])
+    end
+    def destroy
+      @blog = Blog.friendly.find(params[:id])
+      if @blog.destroy
+        redirect_to portfolios_path, notice: "Record was removed"
+      end
+    end
+  
+    private
+  
+    def blog_params
+      params.require(:blog).permit(:title, :body)
+    end
 end
