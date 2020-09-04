dc1cc2d2cd193046bef27271c3b01492dbf6b716
pawelmaryon <maryonpawel@gmail.com>
2020-09-04 10:00:15 +0100

    implemented new and create action for portfoilo

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,23 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
   end
+
   def new
     @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+    if @portfolio_item.save
+      redirect_to portfolios_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :description, :body)
   end
 end
