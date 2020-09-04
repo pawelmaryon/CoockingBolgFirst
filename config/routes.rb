7a0176a404834b8308abc071b369fec37db1d54e
pawelmaryon <maryonpawel@gmail.com>
2020-09-04 15:19:50 +0100

    implemented custon routes for show action for portfoilo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
