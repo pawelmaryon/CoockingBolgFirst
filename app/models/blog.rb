ae53a279de359f4f75bb30986baf539af733cc54
pawelmaryon <maryonpawel@gmail.com>
2020-09-07 08:22:29 +0100

    implenented validations for blog and portfolios

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,6 @@
 class Blog < ApplicationRecord
-
+  validates :title, presence: true
+  validates :body, presence: true
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,8 @@
 class Portfolio < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :body, presence: true
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
