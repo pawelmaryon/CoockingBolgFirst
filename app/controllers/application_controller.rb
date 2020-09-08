9ac5795f841a6011265b15cde7394319c381d0e7
pawelmaryon <maryonpawel@gmail.com>
2020-09-08 14:50:09 +0100

    refactor application controller with set source concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
-
-  before_action :set_session
-
-  def set_session
-    session[:query] = params[:q] if params[:q]
-  end
+  include SetSource
 end
