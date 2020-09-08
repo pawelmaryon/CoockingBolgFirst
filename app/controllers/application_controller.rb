98f7a26146035020f4ff7f6183e8b78dbd0a285c
pawelmaryon <maryonpawel@gmail.com>
2020-09-08 14:36:19 +0100

    Added session tracker for source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+
+  before_action :set_session
+
+  def set_session
+    session[:query] = params[:q] if params[:q]
+  end
 end
