68f21494f9d19e1d7d727a696a6a8b265e5ddd47
pawelmaryon <maryonpawel@gmail.com>
2020-09-20 14:10:11 +0100

    copyright functionality implemented

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,17 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = PMaryonViewTool::Renderer.copyright "Pawel Maryon", "#{@page_title}", "h2"
+  end
+end
+
+module PMaryonViewTool
+  class Renderer
+    def self.copyright name, message, tag_type
+      "&copy; #{Time.now.year} | <#{tag_type}>#{name}</#{tag_type}> #{message}".html_safe
+    end
+  end
 end
