11859d9a6522d3c32f238090cd4ed9a32d929a12
pawelmaryon <maryonpawel@gmail.com>
2020-10-27 11:29:35 +0000

    refactored placeholder functionality

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,19 @@
 module PortfoliosHelper
+  def image_generator(height:, width:)
+    "https://via.placeholder.com/#{height}x#{width}.png"
+  end
+
+  def portfoilo_img(img, type)
+    if img
+      img
+    elsif type=="thumb" 
+      image_generator(height: '350', width: '200')
+    else 
+      image_generator(height: '600', width: '400')
+    end
+  end
 end
+# def set_defaults
+#   self.main_image ||= Placeholder.image_generator(height:600, width:400)
+#   self.thumb_image ||= Placeholder.image_generator(height:350, width:200)
+# end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,7 +1,5 @@
 class Portfolio < ApplicationRecord
-  include Placeholder
-
-  has_many :cousines
+  has_many :cousines, dependent: :delete_all
   accepts_nested_attributes_for :cousines,
                                 reject_if: lambda { |attribute| attribute[:name].blank? }
   validates :title, presence: true
@@ -19,17 +17,4 @@ class Portfolio < ApplicationRecord
     where(description: "Camembert")
   end
   
-  #callback function setting default picture
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_generator(height:600, width:400)
-    self.thumb_image ||= Placeholder.image_generator(height:350, width:200)
-  end
-  # def set_defaults
-  #   if self.main_image == nil
-  #     self.main_image = "https://via.placeholder.com/600x400.png"    
-  #   if self.thumb_image == nil
-  #     self.thumb_image = "https://via.placeholder.com/350x200.png"
-  # end
 end
