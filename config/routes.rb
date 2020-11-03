057e2f5c317fd59044463fbb50b465301f7d7fb1
pawelmaryon <maryonpawel@gmail.com>
2020-11-03 14:27:23 +0000

    layout changes nav-bar and indexpage

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get 'cheese', to: 'portfolios#cheese'
   get 'home', to: 'portfolios#home'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  root to: "portfolios#home"
+  root to: "portfolios#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
