e934515c31ea599d34155be65ca8ff812b795e9b
pawelmaryon <maryonpawel@gmail.com>
2020-11-06 13:27:08 +0000

    title

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -8,7 +8,7 @@ module DefaultPageContent
   end
 
   def page_defaults
-    @page_title = "CookingBolgFirst"
+    @page_title = "Ja Gotuje On Piecze"
     @seo_keywords = "Ja Gotuje On Piecze portfolio"
   end 
   
